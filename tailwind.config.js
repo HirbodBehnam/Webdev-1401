2b7d32c501bd201f237f45a3a326560b5a780973
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2022-11-17 18:52:47 +0330

    Finished login probably Moved colors to CSS Moved link

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        yekan: ["Yekan", "ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
-      }
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                yekan: ["Yekan", "ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
