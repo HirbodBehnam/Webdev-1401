6ffd91974351290340e573320e1a9f3cbd1f0b53
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2022-11-17 10:58:59 +0330

    Added persian

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        yekan: ["Yekan", "ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
+      }
+    },
   },
   plugins: [],
 }
