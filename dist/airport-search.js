0eb5f16f91d6b1c11e9965a0d8ebf37c8d6df40e
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2022-11-25 11:31:21 +0330

    Some small stuff about main page Added sample calendar I want to fucking test something so let's push it

--- dist/airport-search.js
+++ dist/airport-search.js
@@ -10,3 +10,41 @@ function airportSearch(a) {
         return AIRPORTS.slice(0, MAX_AIRPORTS);
     return AIRPORTS.filter((value) => value.toLowerCase().includes(a.toLowerCase())).slice(0, MAX_AIRPORTS);
 }
+
+/**
+ * Must be called when we want to open the search result dialog
+ * @param caller The dom object which calls this method
+ */
+function searchBoxFocusFunction(caller) {
+    const classes = document.getElementById(caller.id + "PlaceholderParent").classList;
+    classes.remove("opacity-0", "scale-95", "ease-in");
+    classes.add("opacity-100", "scale-100", "ease-out");
+    searchBoxHandleSearch(caller);
+}
+
+/**
+ * Must be called when we want to close the search dialog
+ * @param caller The dom object which calls this method
+ */
+function searchBoxBlurFunction(caller) {
+    const classes = document.getElementById(caller.id + "PlaceholderParent").classList;
+    classes.add("opacity-0", "scale-95", "ease-in");
+    classes.remove("opacity-100", "scale-100", "ease-out");
+}
+
+function searchBoxHandleSearch(caller) {
+    const airports = airportSearch(caller.value);
+    const placeholder = document.getElementById(caller.id + "Placeholder");
+    placeholder.innerHTML = "";
+    airports.forEach(airport => {
+        placeholder.innerHTML += `<span
+                               class="search-result-text" role="menuitem" tabindex="-1"
+                               onmousedown="searchBoxMenuItemClicked('${caller.id}','${airport}')">${airport}</span>`;
+    });
+}
+
+function searchBoxMenuItemClicked(toChangeCallerID, value) {
+    const input = document.getElementById(toChangeCallerID);
+    input.value = value;
+    input.blur();
+}
