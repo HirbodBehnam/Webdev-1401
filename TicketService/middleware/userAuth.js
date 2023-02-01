8eba06f99dc38967097d9aade06da18974042468
ErfanMojibi <mojibierfan@gmail.com>
2023-02-01 23:20:18 +0330

    grpc is now working

--- TicketService/middleware/userAuth.js
+++ TicketService/middleware/userAuth.js
@@ -1,6 +1,6 @@
 const grpc = require("@grpc/grpc-js");
 let protoLoader = require("@grpc/proto-loader");
-const PROTO_PATH = __dirname + "/proto/auth.proto";
+const PROTO_PATH = __dirname + "/proto/auth_service.proto";
 
 const options = {
     keepCase: true,
@@ -12,22 +12,21 @@ const options = {
 
 let packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
 
-const authService = grpc.loadPackageDefinition(packageDefinition).AuthServerService;
+const authService = grpc.loadPackageDefinition(packageDefinition).proto.AuthServerService;
 
 const client = new authService(
     "localhost:21901",
     grpc.credentials.createInsecure()
 );
-
 const validateToken =  (req, res, next) => {
     console.log("headers", req.headers);
     const token = req.headers.authorization.substring(7);
     console.log("token", token);
     client.AuthorizeToken({ token: token }, (err, response) => {
-        console.log(response)
-        console.log(err)
+
         if(response){
             req.user = response;
+            console.log("response", response);
             next()
         } else {
             res.json({msg:"invalid token"});
