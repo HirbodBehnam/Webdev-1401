ed3b06f3c7cb39fd98608791280cb340d65aecb4
Soroush Sherafat <sorousherafat@gmail.com>
2023-02-03 06:13:17 +0330

    added `purchaseTicket` controller function prototype

--- TicketService/controllers/purchasesController.js
+++ TicketService/controllers/purchasesController.js
@@ -10,4 +10,15 @@ const getPurchases = async (req, res) => {
   res.json({ message: result });
 };
 
+const purchaseTicket = async (req, res) => {
+  // todo make sure the user is authenticated
+  // get user ID, flight ID and flight class
+  // get the `available_offers` view
+  // check if 1) the flight class has capacity and 2) it's not past the departure time UTC
+  // get the flight price based on type
+  // do the transaction
+  // if the transaction was successful, create a new `purchase` object
+  // show the response to user
+}
+
 module.exports = getPurchases;
