6fd330bf0ea2d1a3813d36cec2f462416d4c49ea
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2023-02-17 10:17:44 +0330

    Added grpc for ticket core Added docs Added ping for database in auth core

--- AuthCore/internal/database/pg.go
+++ AuthCore/internal/database/pg.go
@@ -2,10 +2,19 @@ package database
 
 import (
 	"context"
+
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 // NewPostgresDatabase will connect to a postgres database
 func NewPostgresDatabase(connectionString string) (*pgxpool.Pool, error) {
-	return pgxpool.New(context.Background(), connectionString)
+	pool, err := pgxpool.New(context.Background(), connectionString)
+	if err != nil {
+		return nil, err
+	}
+	err = pool.Ping(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	return pool, nil
 }
